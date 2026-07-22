0afdeec2173283742dd48f7d2b6025da796ed752
Edwin Joye <edwinjoye@gmail.com>
2026-07-22 18:54:42 +0200

    style(dashboard): use brand color for segmented control icons on desktop

--- src/components/dashboard/DashboardToolbar.tsx
+++ src/components/dashboard/DashboardToolbar.tsx
@@ -63,11 +63,23 @@ function DashboardToolbarDesktop({
           onChange={onViewChange}
           data={[
             {
-              label: <SegmentedItemLabel icon={GlobeIcon} label="Global" />,
+              label: (
+                <SegmentedItemLabel
+                  icon={GlobeIcon}
+                  label="Global"
+                  iconColor="var(--mantine-color-brand-6)"
+                />
+              ),
               value: "global",
             },
             {
-              label: <SegmentedItemLabel icon={FilmSlateIcon} label="Movie" />,
+              label: (
+                <SegmentedItemLabel
+                  icon={FilmSlateIcon}
+                  label="Movie"
+                  iconColor="var(--mantine-color-brand-6)"
+                />
+              ),
               value: "movie",
             },
           ]}
